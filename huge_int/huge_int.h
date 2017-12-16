dc524dafe0f83573b7dd14d81f32ce8e0d4dc583
zcy <1013629758@qq.com>
2017-12-15 19:41:29 -0800

    huge int

---
+++ huge_int/huge_int.h
@@ -0,0 +1,21 @@
+#ifndef _HUGE_INT_H_INCLUDED
+#define _HUGE_INT_H_INCLUDED
+
+#include <deque>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::deque;
+using std::vector;
+using std::istream;
+using std::ostream;
+using std::string;
+
+
+
+
+
+
+
+#endif
