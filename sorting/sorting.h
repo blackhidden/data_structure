2fd7c4cc6a798623f522eaf25e69ab4475933ed1
blackhidden <13516355198@163.com>
2020-02-19 17:27:02 +0800

    fix bugs

--- sorting/sorting.h
+++ sorting/sorting.h
@@ -1,28 +1,47 @@
 #pragma once
 
+/**
+ * 	排序算法稳定性：保证排序前后两个相等的数在序列中的相对位置相同
+ * 
+ * 	稳定排序：
+ * 		冒泡排序、插入排序、归并排序		
+ * 
+ * 	不稳定排序：
+ * 		选择排序、希尔排序、堆排序、快排、基数排序
+ **/ 
+
 //---------------bubble sort----------------------
+/**
+ * 	时间复杂度：O(n^2)
+ * 	空间复杂度：O(1)
+ **/ 
 template<typename T>
 void bubble_sort(T a[], int n) {
 	for (int i = 0; i < n; ++i) {
-		for (int j = n-1; j > i; --j) {
-			if (a[j] < a[j-1]) {
+		for (int j = n - 1; j > i; --j) {
+			if (a[j] < a[j - 1]) {
 				T tmp = a[j];
-				a[j] = a[j-1];
-				a[j-1] = tmp;
+				a[j] = a[j - 1];
+				a[j - 1] = tmp;
 			}
 		}
 	}
 }
 
 //---------------select sort---------------------
+/**
+ * 	时间复杂度：O(n^2)
+ * 	空间复杂度：O(1)
+ **/ 
 template<typename T>
 void select_sort(T a[], int n) {
 	int minIndex = 0;
-	for (int i = 0; i < n-1; ++i) {
+	for (int i = 0; i < n - 1; ++i) {
 		minIndex = i;
-		for (int j = i+1; j < n; ++j) {
-			if (a[j] < a[minIndex])
+		for (int j = i + 1; j < n; ++j) {
+			if (a[j] < a[minIndex]) {
 				minIndex = j;
+			}
 		}
 		if (minIndex != i) {
 			T tmp = a[minIndex];
@@ -33,30 +52,39 @@ void select_sort(T a[], int n) {
 }
 
 //---------------insertion sort-------------------
+/**
+ * 	时间复杂度：O(n^2)
+ * 	空间复杂度：O(1)
+ **/ 
 template<typename T>
 void insertion_sort(T a[], int n) {
-	T tm;
-	for (int i = 0; i != n; ++i) {
-		tm = a[i];
-		int k = i;
-		for (; k > 0 && a[k-1] > tm; --k) {
-			a[k] = a[k-1];
+	T tmp;
+	for (int i = 0; i < n; ++i) {
+		tmp = a[i];
+		int j = i;
+		for (; j > 0 && a[j - 1] > tmp; --j) {
+			a[j] = a[j - 1];
 		}
-		a[k] = tm;
+		a[j] = tmp;
 	}
 }
 
 //---------------shell sort-------------------------
+/**
+ * 	时间复杂度：O(n^2)
+ * 	空间复杂度：O(1)
+ *  改进的插入排序，当n越大时，效果越明显
+ **/ 
 template<typename T>
 void shell_sort(T a[], int n) {
-	for (int increment = n/2; increment >= 1; increment /= 2) {
-		if (increment %2 == 0) increment++;
+	for (int increment = n / 2; increment >= 1; increment /= 2) {
+		if (increment % 2 == 0) increment++;
 		for (int i = increment; i < n; ++i) {
 			T tm = a[i];
 			int j = i;
 			for (; j >= increment; j -= increment) {
-				if (tm >= a[j-increment]) break;
-				a[j] = a[j-increment];
+				if (tm >= a[j - increment]) break;
+				a[j] = a[j - increment];
 			}
 			a[j] = tm;
 		}
@@ -64,12 +92,21 @@ void shell_sort(T a[], int n) {
 }
 
 //-------------------------heap sort-----------------------------
+/**
+ * 	时间复杂度：O(nlogn)
+ * 	空间复杂度：O(1)
+ *  堆是完全二叉树
+ *  大顶堆：每个结点的值都大于或等于其左右孩子结点的值
+ *  小顶堆：每个结点的值都小于或等于其左右孩子结点的值
+ *  建堆（小顶堆）：从第一个非叶子节点开始，选其较小子节点，进行比较交换。
+ *  堆排序：首先建立小顶堆，然后出堆顶元素，在建立小顶堆，依次进行。
+ **/
 template<typename T>
 void percDown(T a[], int n, int i) {
 	T tm = a[i];
-	int child = i<<1;//leftchild(i) = 2 * i + 1
-	for (; child < n-1; i = child, child = (child<<1)) {
-		if (child != n-1 && a[child] > a[child+1])
+	int child = i << 1;  //leftchild(i) = 2 * i + 1
+	for (; child < n - 1; i = child, child = child << 1) {
+		if (child != n - 1 && a[child] > a[child + 1])
 			child++;
 		if (tm > a[child])
 			a[i] = a[child];
@@ -88,10 +125,10 @@ void reverse(T a[], int n) {
 }
 
 template<typename T>
-void heap_sort(T a[], int n) {
-	for (int i = n/2; i >= 0; --i)
+void headp_sort(T a[], int n) {
+	for (int i = n / 2; i >= 0; --i) 
 		percDown(a, n, i);//build heap
-	for (int j = n-1; j >= 0; --j) {
+	for (int j = n - 1; i >= 0; --j) {
 		T min = a[0];//delete min
 		a[0] = a[j];
 		a[j] = min;
@@ -101,34 +138,39 @@ void heap_sort(T a[], int n) {
 }
 
 //-------------------------merge sort recursive-----------------------------
+/**
+ * 	时间复杂度：O(nlogn)
+ * 	空间复杂度：O(n)
+ **/
 template<typename T>
 void merge_array(T a[], int beg, int end) {
 	int center = (beg + end) / 2;
-	for (int i = beg, j = center+1; i <= end && j <= end;) {
+	for (int i = beg, j = center + 1; i <= end && j <= end;) {
 		if (a[i] > a[j]) {
-			T tm = a[j];
-			for (int k = j; k != i; --k)
-				a[k] = a[k-1];
+			T tm = a[i];
+			a[i] = a[j];
+			for (int k = j; k > i; --k) 
+				a[k] = a[k - 1];
 			a[i] = tm;
-			++j;++i;
-		}
-		else ++i;
+			++j; ++i;
+		} else 
+			++i;
 	}
 }
 
 template<typename T>
-void m_sort(T a[], int left, int right) {
-	if (left < right) {
-		int center = (left + right) / 2;
-		m_sort(a, left, center);
-		m_sort(a, center+1, right);
-		merge_array(a, left, right);
+void m_sort(T a[], int beg, int end) {
+	if (beg < end) {
+		int center = (beg + end) / 2;
+		m_sort(a, beg, center);
+		m_sort(a, center + 1, end);
+		merge_array(a, beg, end);
 	}
 }
 
 template<typename T>
 void merge_sort_rec(T a[], int n) {
-	m_sort(a, 0, n-1);
+	m_sort(a, 0, n - 1);
 }
 
 //-------------------------merge sort non-recursive-------------------------
